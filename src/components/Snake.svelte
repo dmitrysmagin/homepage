8177624509133fcb51370d8cb28d6accd2c2d904
Damien Smart <damien.smart@bbconsult.co.uk>
2022-10-12 20:54:06 +0300

    Center canvas

--- src/components/Snake.svelte
+++ src/components/Snake.svelte
@@ -2,12 +2,26 @@
     const UNIQUEHASH = (Math.random() + 1).toString(36).substring(7);
  </script>
 
+ <style global>
+    #game {
+        image-rendering: -moz-crisp-edges;
+        image-rendering: -webkit-crisp-edges;
+        image-rendering: pixelated;
+        /*width: 600px;
+        height: 600px;*/
+        border: 2px solid purple;
+        border-radius: 8px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+ </style>
+
 <svelte:head>
     <script src="/js/snake.js"></script>
 </svelte:head>
 
 <!-- Trick to force static script -->
-<div hash={UNIQUEHASH}>
+<div hash={UNIQUEHASH} style="text-align: center;">
     <p>Press button to run js code!</p>
     <canvas width="400" height="400" id="game" style="display: none;"></canvas>
     <button id="button_run">RUN</button>
