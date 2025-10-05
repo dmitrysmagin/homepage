0eb6419601be90288ce687ad13756e106b518c11
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2025-10-05 13:35:54 +0300

    Improve layout

--- src/components/Navigation/NavigationBottom.svelte
+++ src/components/Navigation/NavigationBottom.svelte
@@ -24,7 +24,7 @@
 <Footer class="rounded-[0] sticky start-0 bottom-0 z-20 w-full border-t border-gray-200 bg-white p-4 shadow-sm md:flex md:items-center md:justify-between md:p-6 dark:border-gray-600 dark:bg-gray-800">
     <!--<FooterCopyright href="/" by="Flowbite™" year={2025}/>-->
     <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        Created with <A class="text-inherit" href="https://astro.build/">Astro</A>, Tailwind/Flowbite.
+        Created with <A class="text-inherit" href="https://astro.build/">Astro</A>, Tailwind/Flowbite and Svelte.
     </span>
     <FooterLinkGroup class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
         <FooterLink href="/">About</FooterLink>
