d0128ec591a3779773044b40cfc7653ecb12ef77
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2025-10-07 23:45:10 +0300

    Rename Games2.svelte -> Games.svelte

--- src/components/Games.svelte
+++ src/components/Games.svelte
@@ -1,81 +1,34 @@
-<script>
+<script lang="ts">
+    // https://codepen.io/knownji/pen/QwLGRBg?editors=1000
+
+    import CarouselStatic from "./CarouselStatic.svelte";
+    import { P, A } from "flowbite-svelte";
+
+    const mission_images = [
+        "/img/lastmission00.png",
+        "/img/lastmission01.png",
+        "/img/lastmission02.png",
+        "/img/lastmission03.png",
+        "/img/lastmission04.png",
+        "/img/lastmission05.png"
+    ];
+
+    const wetspot_images = [
+        "/img/wetspot2-06.png",
+        "/img/wetspot2-01.png",
+        "/img/wetspot2-02.png"
+    ];
 </script>
 
 <style>
-    .panel {
-        display: flex;
-        flex-direction: row;
-        justify-content: stretch;
-        background-color: white;
-        /*max-height: 930px;*/
-        width: inherit;
-    }
-
-    .left_panel {
-        flex-shrink: 0;
-        max-width: 336px; /* 320px for image and + 16 for scroller */
-        overflow-x: hidden;
-        overflow-y: hidden;
-
-    }
-
-    .left_panel img {
-        opacity: 1;
-        transition: opacity 0.2s ease-in;
-    }
-
-    .left_panel img:hover {
-        opacity: 0.8;
-    }
-
-    .left_panel,
-    .right_panel {
-        border: 1px solid lightgray;
-    }
-
-    .right_panel {
-        width: -webkit-fill-available;
-        /* overflow-y: hidden; */
-    }
-
-    .description {
-        padding: 10px;
-    }
-
-    @media (max-width: 960px) {
-        .panel {
-            flex-direction: column;
-            max-height: unset;
-        }
-
-        .panel > .left_panel {
-            display: flex;
-            flex-direction: row;
-            flex-shrink: 1;
-            max-width: unset;
-            white-space: nowrap;
-            overflow-x: scroll;
-            overflow-y: hidden;
-        }
-
-        .panel > .right_panel {
-            overflow-y: unset;
-        }
-    }
 </style>
 
-<section class="max-w-3xl mx-auto d-flex flex-row">
-    <div class="panel">
-        <div class="left_panel">
-            <img src="/img/lastmission00.png" alt="screenshot" width="320" />
-            <img src="/img/lastmission01.png" alt="screenshot" width="320" />
-            <img src="/img/lastmission02.png" alt="screenshot" width="320" />
-            <img src="/img/lastmission03.png" alt="screenshot" width="320" />
-            <img src="/img/lastmission04.png" alt="screenshot" width="320" />
-            <img src="/img/lastmission05.png" alt="screenshot" width="320" />
-        </div>
-        <div class="right_panel description">
-            <h1>The Last Mission Remake</h1>
+<section class="max-w-3xl mx-auto">
+    <div class="w-full">
+        <CarouselStatic images={mission_images} />
+
+        <div class="description">
+            <h2>The Last Mission Remake</h2>
             <p>
                 Originally this game was released in 1987 by Spanish company Opera Soft for multiple 8-bit computers
                 including MSX1, MSX2, ZX-Spectrum, Amstrad CPC and even x86 PC. As most games of that era, The Last
@@ -95,49 +48,45 @@
                 effects and free music by Mark Braga.<br />
                 Most probably, iOS version is nowhere to be found now, sorry for that.
             </p>
-            <p>
+            <P>
                 <b>Authors</b><br />
                 Pedro Ruiz - the original author and Opera Soft founder<br />
                 Dmitry Smagin - main coding<br />
                 Alexey Pavlov - additional coding and gfx, porting to iOS devices<br />
                 Mark Braga - music and sfx<br />
-            </p>
+            </P>
             <p>
                 <b>Downloads</b><br />
-                <a href="https://github.com/dmitrysmagin/last-mission/releases/download/v0.8/last-mission-win-0.8.zip"
-                    >last-mission-win-0.8.zip</a>
+                <A href="https://github.com/dmitrysmagin/last-mission/releases/download/v0.8/last-mission-win-0.8.zip"
+                    >last-mission-win-0.8.zip</A>
                 - Pre-compiled binary for win95 or later<br />
-                <a
+                <A
                     href="https://github.com/dmitrysmagin/last-mission/releases/download/v0.8/last-mission-linux-0.8.tar.gz"
-                    >last-mission-linux-0.8.tar.gz</a>
+                    >last-mission-linux-0.8.tar.gz</A>
                 - Pre-compiled binary for linux (Ubuntu 32-bit)<br />
-                <a href="https://github.com/dmitrysmagin/last-mission/releases/download/v0.8/last-mission.opk"
-                    >last-mission.opk</a>
+                <A href="https://github.com/dmitrysmagin/last-mission/releases/download/v0.8/last-mission.opk"
+                    >last-mission.opk</A>
                 - Binary package for Opendingux devices (GCW-Zero, RG-350 etc)<br />
-                <a href="https://github.com/dmitrysmagin/last-mission/archive/v0.8.tar.gz"
-                    >last-mission-0.8-src.tar.gz</a>
+                <A href="https://github.com/dmitrysmagin/last-mission/archive/v0.8.tar.gz"
+                    >last-mission-0.8-src.tar.gz</A>
                 - Source code package<br />
-                <a href="https://github.com/dmitrysmagin/last-mission" target="_blank">Github page</a>
+                <A href="https://github.com/dmitrysmagin/last-mission" target="_blank">Github page</A>
                 - The 'master' branch contains version 0.8 and 'unstable' has an unfinished level editor with lots of code
                 improvements, though totally identical gameplay-wise.<br />
             </p>
             <p>
                 <b>Other media</b><br />
-                <a href="/posts/2014-08-17-making-of-the-last-mission">Making of 'The Last Mission' remake</a>
-                article on <a href="https://web.archive.org/web/20220525193228/http://gcw-zero.com/" target="_blank">www.gcw-zero.com</a>
+                <A href="/posts/2014-08-17-making-of-the-last-mission">Making of 'The Last Mission' remake</A>
+                article on <A href="https://web.archive.org/web/20220525193228/http://gcw-zero.com/" target="_blank">www.gcw-zero.com</A>
                 (now defunct)<br />
-                <a href="https://www.mobygames.com/game/last-mission/" target="_blank">Mobygames page</a>
+                <A href="https://www.mobygames.com/game/last-mission/" target="_blank">Mobygames page</A>
             </p>
         </div>
     </div>
     <div class="panel">
-        <div class="left_panel">
-            <img src="/img/wetspot2-06.png" alt="screenshot" width="320" />
-            <img src="/img/wetspot2-01.png" alt="screenshot" width="320" />
-            <img src="/img/wetspot2-02.png" alt="screenshot" width="320" />
-        </div>
-        <div class="right_panel description">
-            <h1>Wetspot II</h1>
+        <CarouselStatic images={wetspot_images} />
+        <div class="description">
+            <h2>Wetspot II</h2>
             <p>
                 Originally Wetspot II was written by Angelo Mottola (Enhanced Creations) in 1997-98. Despite being
                 programmed in QuickBasic 4.5 it used some low-level techniques like machine code injecting, EMS memory
@@ -152,7 +101,7 @@
             <p>
                 Currently you can play the original game using Dosbox if you are lucky enough to find the latest version
                 and set everything up correctly. Luckily, the original site was archived and is available
-                <a href="https://geocities.restorativland.org/SiliconValley/Lakes/7303/" target="_blank">here</a>.
+                <A href="https://geocities.restorativland.org/SiliconValley/Lakes/7303/" target="_blank">here</A>.
             </p>
             <p>
                 This is a full re-implementation of game engine entirely in C using SDL library written in 2013-2014.
@@ -160,9 +109,9 @@
                 sound effects and even adlib midi music were preserved to keep the old DOS feel.
             </p>
             <p>
-                Initially this remake was planned as an exclusive for GCW-Zero handheld and was <a
-                    href="http://www.gcw-zero.com/news.php?id=8"
-                    target="_blank">released</a> for it with different music due to copyright concerns:
+                Initially this remake was planned as an exclusive for GCW-Zero handheld and was
+                <A href="http://www.gcw-zero.com/news.php?id=8" target="_blank">released</A>
+                for it with different music due to copyright concerns:
             </p>
             <p>
                 Some differences with the original game:<br />
@@ -176,16 +125,17 @@
             </p>
             <p>
                 <b>Downloads</b><br />
-                <a href="https://github.com/dmitrysmagin/wetspot2/releases/download/v0.9/wetspot2-sdl2-win32.zip"
-                    >wetspot2-sdl2-win32.zip</a>
+                <A href="https://github.com/dmitrysmagin/wetspot2/releases/download/v0.9/wetspot2-sdl2-win32.zip"
+                    >wetspot2-sdl2-win32.zip</A>
                 - Pre-compiled binary for windows<br />
-                <a href="https://github.com/dmitrysmagin/wetspot2/archive/v0.9.zip" rel="nofollow">wetspot2-0.9.zip</a>
+                <A href="https://github.com/dmitrysmagin/wetspot2/archive/v0.9.zip" rel="nofollow">wetspot2-0.9.zip</A>
                 - Source code package<br />
             </p>
             <p>
                 <b>Other media:</b><br />
-                <a href="https://github.com/dmitrysmagin/wetspot2">Wetspot II git page</a>
+                <A href="https://github.com/dmitrysmagin/wetspot2">Wetspot II git page</A>
             </p>
         </div>
     </div>
+
 </section>
